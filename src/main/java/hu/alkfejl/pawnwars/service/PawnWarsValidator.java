2389b4e525680aa94ef504859d14ea5b70bc9060
Facenoob <noma0022@gmail.com>
2021-04-17 23:09:21 +0200

    Update PawnWarsValidator.java

--- src/main/java/hu/alkfejl/pawnwars/service/PawnWarsValidator.java
+++ src/main/java/hu/alkfejl/pawnwars/service/PawnWarsValidator.java
@@ -3,6 +3,9 @@ package hu.alkfejl.pawnwars.service;
 import hu.alkfejl.pawnwars.model.Pawn;
 import org.springframework.stereotype.Service;
 
+import java.awt.*;
+import java.util.LinkedList;
+
 @Service
 public class PawnWarsValidator {
     public boolean validateInput(String[] input) {
@@ -19,9 +22,32 @@ public class PawnWarsValidator {
         return board[Integer.parseInt(input[0])][Integer.parseInt(input[1])] != null;
     }
 
-    public boolean validateStep() {
+    public boolean validateNoPawn(String[] input) {
+        final Pawn[][] board = PawnWarsService.getBoard();
+        return board[Integer.parseInt(input[2])][Integer.parseInt(input[3])] == null;
     }
 
+    public boolean diagonalValid(String[] input) {
+        final Pawn[][] board = PawnWarsService.getBoard();
+        return board[Integer.parseInt(input[0]) - 1][Integer.parseInt(input[1])+1] == null;
+        /*átlósan kell nézni a mezőket. Ha a közvetlen jobb/bal átlón van azonos színű akkor abban az irányban lehetnek jó lépések.
+        Ezt kb addig kell ellenőrizni amíg lehetnek lehetséges lépesek az átlókon*/
+    }
+
+    public List possibleMoves(String[] input) {
+        LinkedList<String> moves = new LinkedList<String>();
+        final Pawn[][] board = PawnWarsService.getBoard();
+        if (board[Integer.parseInt(input[0]) -1][Integer.parseInt(input[1])] == null) {
+            moves.add(String.valueOf(Integer.parseInt(input[0])-1));//előre már tudunk lépni ha minden igaz
+        }
+
+
+    }
 }
+
+    /*public boolean validateStep() {
+    }*/
+
+
 
 
