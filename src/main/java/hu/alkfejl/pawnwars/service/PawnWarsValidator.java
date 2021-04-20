88dddfdf4f53fb0fa3670f765fcfd32cf8d8017d
Facenoob <noma0022@gmail.com>
2021-04-20 22:42:28 +0200

    Lépés validálók
    Az átlós lépéseket nem tudom megkeresni, meg abban nem vagyok biztos hogy a tábla módosítás mennyire helyes.

--- src/main/java/hu/alkfejl/pawnwars/service/PawnWarsValidator.java
+++ src/main/java/hu/alkfejl/pawnwars/service/PawnWarsValidator.java
@@ -3,7 +3,7 @@ package hu.alkfejl.pawnwars.service;
 import hu.alkfejl.pawnwars.model.Pawn;
 import org.springframework.stereotype.Service;
 
-import java.awt.*;
+import java.sql.Array;
 import java.util.LinkedList;
 
 @Service
@@ -26,27 +26,38 @@ public class PawnWarsValidator {
         final Pawn[][] board = PawnWarsService.getBoard();
         return board[Integer.parseInt(input[2])][Integer.parseInt(input[3])] == null;
     }
-
+    //ez a rész ami nekem feladta leckét de nagyon
     public boolean diagonalValid(String[] input) {
         final Pawn[][] board = PawnWarsService.getBoard();
-        return board[Integer.parseInt(input[0]) - 1][Integer.parseInt(input[1])+1] == null;
-        /*átlósan kell nézni a mezőket. Ha a közvetlen jobb/bal átlón van azonos színű akkor abban az irányban lehetnek jó lépések.
-        Ezt kb addig kell ellenőrizni amíg lehetnek lehetséges lépesek az átlókon*/
+        int index = 0;
+        for (int i = Integer.parseInt(input[0]) - 1; i > 0; i--) {
+
+        }
     }
 
-    public List possibleMoves(String[] input) {
+    public LinkedList<String> possibleMoves(String[] input) {
         LinkedList<String> moves = new LinkedList<String>();
         final Pawn[][] board = PawnWarsService.getBoard();
-        if (board[Integer.parseInt(input[0]) -1][Integer.parseInt(input[1])] == null) {
-            moves.add(String.valueOf(Integer.parseInt(input[0])-1));//előre már tudunk lépni ha minden igaz
+        if (board[Integer.parseInt(input[0]) - 1][Integer.parseInt(input[1])] == null) {
+            moves.add(String.valueOf(Integer.parseInt(input[0]) - 1));//előre már tudunk lépni ha minden igaz
         }
+        return moves;
 
+    }
 
+    public boolean validateStep(String[] input, LinkedList moves) {
+        return moves.contains(input);
+    }
+
+    public Pawn[][] Stepper(String[] fromInput, String[] toInput) {
+        final Pawn[][] board = PawnWarsService.getBoard();
+        board[Integer.parseInt(toInput[0])][Integer.parseInt(toInput[1])] = board[Integer.parseInt(fromInput[0])][Integer.parseInt(fromInput[1])];
+        board[Integer.parseInt(fromInput[0])][Integer.parseInt(fromInput[1])] = null;
+        return board;
     }
 }
 
-    /*public boolean validateStep() {
-    }*/
+
 
 
 
