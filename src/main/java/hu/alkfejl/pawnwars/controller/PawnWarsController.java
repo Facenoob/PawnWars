6ebbf10c39087b15fa957a2de9e927ec528b0e4e
Facenoob <noma0022@gmail.com>
2021-04-25 15:41:02 +0200

    Controller upgrade
    A Post mapping és get mapping. Lépés a táblán.

--- src/main/java/hu/alkfejl/pawnwars/controller/PawnWarsController.java
+++ src/main/java/hu/alkfejl/pawnwars/controller/PawnWarsController.java
@@ -1,11 +1,12 @@
 package hu.alkfejl.pawnwars.controller;
+
 import hu.alkfejl.pawnwars.service.PawnWarsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class PawnWarsController {
@@ -13,16 +14,23 @@ public class PawnWarsController {
     private PawnWarsService pawnWarsService;
 
 
-    @GetMapping(value = "/board")
-    public String board(Model model) {
+    @PostMapping(value = "/create")
+    public String create(Model model) {
         model.addAttribute("board", pawnWarsService.createBoard());
         return "board";
     }
 
-    @PatchMapping(value ="/step")
-    public void step(String from,String to){
-        pawnWarsService.handleStep(from,to);
+    @PostMapping(value = "/step")
+    public String step(String from, String to,Model model) {
+        pawnWarsService.handleStep(from, to);
+        model.addAttribute("board", PawnWarsService.getBoard());
+        return "board";
+    }
 
+    @GetMapping(value = "/board")
+    public String board(Model model) {
+        model.addAttribute("board", PawnWarsService.getBoard());
+        return "board";
     }
 
 
